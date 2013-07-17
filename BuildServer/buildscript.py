42c2787291fd51f9ebffb229c745175c8568c5bf
Connor <connor.n.bell@gmail.com>
2013-07-17 17:39:05 -0300

    Initial build script commit

---
+++ BuildServer/buildscript.py
@@ -0,0 +1,128 @@
+import os
+import git
+import subprocess
+import zipfile
+import sys
+import shutil
+import optparse
+
+buildPlatforms = []
+gitDirectory = ""
+windowsUnityPath = "C:\Program Files (x86)\Unity\Editor\Unity.exe"
+macUnityPath = "/Applications/Unity/Unity.app/Contents/MacOS/Unity"
+supportedPlatforms = ["mac", "windows", "ios"];
+
+def makebuild(platform):
+	
+	unityPath = ""
+	methodName = ""
+	
+	if os.path.isfile(macUnityPath):
+		unityPath = macUnityPath
+	elif os.path.isfile(windowsUnityPath):
+		unityPath = windowsUnityPath
+	else:
+		print "Error: Unity not found"
+		return
+	
+	if platform == 'mac':
+		methodName = "BuildAutomation.MakeMacBuild"
+	if platform == 'windows':
+		methodName = "BuildAutomation.MakeWindowsBuild"
+	if platform == 'ios':
+		methodName = "BuildAutomation.MakeiOSBuild"
+	
+	returnCode = subprocess.call([unityPath, "-projectPath", projectDirectory, "-executeMethod", methodName, "-headless", "-quit"])
+
+	if returnCode != 0:
+		print "Unity build failed"
+		quit()
+
+def zipdir(dir, zip):
+	assert(isinstance(zip,ZipFile) == True)
+		
+	for root, dirs, files in walk(dir):
+		for file in files:
+			zip.write(path.join(root, file))
+
+def findHeadForBranchName(branch, repoHeads):
+	for head in repoHeads:
+		if head.name == branch:
+			return head
+
+def buildNeededBranchesForRepo(repo, branches):
+
+	for branch in branches:
+		head = findHeadForBranchName(branch, repo.heads)
+		if head is not None:
+			currentCommit = head.commit
+			head.checkout()
+			head.pull()
+			newCommit = head.commit
+
+			if currentCommit != newCommit:
+				for platform in buildPlatforms:
+					makebuild(platform)
+				
+		else:
+			print "Branch " + branch + " doesn't exist"
+
+def makezipfiles():
+	files = filesInCurrentDirectory()
+	for file in files:
+		zipfilename = (file + '.zip')
+		if not os.path.isfile(zipfilename):
+			zip = zipfile.ZipFile(zipfilename, 'w')
+			zipdir(file, zip)
+			zip.close()
+			shutil.rmtree(file)
+
+def compilexcodeproject():
+	files = filesInCurrentDirectory()
+	for file in files:
+		if not os.path.isfile(file):
+			os.chdir(os.path.join(path.abspath(sys.path[0]), file))
+			retValue = subprocess.call (["xcodebuild"])
+			if retValue != 0:
+				print "XCode build failed."
+				quit()
+
+def filesInCurrentDirectory():
+	return [file for file in listdir('.') if not os.path.isfile(file)]
+
+def parseArguments(parser):
+	parser.add_option("-p", "--platform", dest="platforms", help="Choose detination platform(s). Options: " + ', '.join(supportedPlatforms), metavar="PLATFORM", action="append")
+	parser.add_option("-r", "--repo", dest="repo", help="Directory repo to build", metavar="DIRECTORY")
+	parser.add_option("-b", "--branches", dest="branches", help="One or more branches to checkout and make builds from", action="append")
+
+	return parser.parse_args()
+
+def validatePlatformArguments(platforms):
+	for platform in platforms:
+		if platform not in supportedPlatforms:
+			parser.error("Platform is not supported. Try ios, mac, or windows")
+			quit()
+
+if __name__ == '__main__':
+
+	parser = optparse.OptionParser()
+	(options, args) = parseArguments(parser)
+	
+	gitDirectory = options.repo
+	buildPlatforms = options.platforms
+	validatePlatformArguments(buildPlatforms)
+	
+	if os.path.exists(gitDirectory):
+		
+		repo = git.Repo(gitDirectory)
+		buildNeededBranchesForRepo(repo, options.branches)
+		
+		for platform in options.branches:
+			if options.platform == 'mac' or options.platform == 'windows':
+				makezipfiles()
+			if options.platform == 'ios':
+				compilexcodeproject()
+
+		print " "
+	else:
+		print "Error: Git repo does not exist: %s" % gitDirectory

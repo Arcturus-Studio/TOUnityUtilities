80daa15b97a81d62ba0426dc04cd9cfa27856574
Devin Horsman <devin@twistedoakstudios.com>
2013-07-23 22:30:52 -0300

    Update SpecialCoroutines.cs
    Added OverTime function to specialCoroutines

--- Assets/TOUtilities/SpecialCoroutines.cs
+++ Assets/TOUtilities/SpecialCoroutines.cs
@@ -3,10 +3,26 @@ using System;
 using System.Collections;
 
 public static class MonoBehaviorExt{
-  public static Coroutine<T> StartCoroutine<T>(this MonoBehaviour obj, IEnumerator coroutine){
+	public static Coroutine<T> StartCoroutine<T>(this MonoBehaviour obj, IEnumerator coroutine){
 		Coroutine<T> coroutineObject = new Coroutine<T>();
 		coroutineObject.coroutine = obj.StartCoroutine(coroutineObject.InternalRoutine(coroutine));
 		return coroutineObject;
+	}
+	
+	public static IEnumerator OverTime(
+		this MonoBehaviour obj,
+		float time, 
+	    Func<float, float> f,
+		Action<float> action){
+		
+		float startTime = Time.time;
+		while(Time.time - startTime < time){
+			float u = f((Time.time - startTime)/time);
+			action(u);
+			yield return null;
+		}
+		action(f(1));
+		yield break;
 	}
 }
 
